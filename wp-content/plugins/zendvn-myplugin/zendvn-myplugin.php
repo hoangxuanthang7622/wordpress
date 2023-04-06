3760f39d003b3cee75ea866e22cd70f5297f9021
unknown <hoangxuanthang7622@gmail.com>
2023-04-06 10:00:49 +0700

    xây dựng plugin căn bản-Path

--- wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
+++ wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
@@ -7,3 +7,11 @@
 // Version: 1.0
 // Author URI: http://www.zen.vn
 // */
+// $path = dirname(__FILE__) . '/includes/admin.php';
+// echo '<br/>' . $path;
+$path = dirname(__FILE__) . '/includes/admin.php';
+// echo '<br/>' . plugin_dir_path(__FILE__ ) ; //lấy ra đường dẫn tuyệt đối đến thư mục plugin
+// echo '<br/>' . plugins_url('/css/abc.css','zendvn',__FILE__);
+// echo '<br/>' . includes_url('/css/buttons-rtl.css');
+// echo '<br/>' . content_url('/themes/twentytwentyone/style-rtl.css');
+// admin_url();
